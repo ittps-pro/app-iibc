7ddf4683727d3929d7631cfc5328619c2430b663
ittps-pro <tpa@ittps.pro>
2024-04-02 15:52:21 +0300

    1

---
+++ vite/src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+
+
+let items = ["ii7566612499"]
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+
+
+
+    <App />
+  </React.StrictMode>,
+)
