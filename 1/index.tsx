d3a67327496d0b0c877bc982cf18bd46296b4533
ittps-pro <tpa@ittps.pro>
2024-04-02 17:12:04 +0300

    d

---
+++ 1/index.tsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import Demo from './demo';
+
+// createRoot(document.getElementById('container')).render(<Demo />);
+  
+
+const app = require('./vite')
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-
-
-
-let items = ["ii7566612499"]
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
-
-
+    
     <App />
   </React.StrictMode>,
 )
